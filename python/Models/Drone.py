c146c8c93e06aef752f4e52e77d91632e8198842
sdegefa <sdegefa2020@fau.edu>
2023-12-02 10:25:01 -0500

    Drone dataclass created, working on drone controller currently

--- python/Models/Drone.py
+++ python/Models/Drone.py
@@ -1,8 +1,19 @@
-from dataclasses import dataclass
-
+from dataclasses import dataclass, field
 
 @dataclass
-Class Drone:
+class Drone:
+    # ID of drone, drones will be created robot_{ID}
+    droneID: int
+    
+    # Either Ready (Spawned in and Stationary), Driving, 
+    # or Dead (despawned or destroyed)
+    currentStatus: str = "Dead"
+    
+    # location list: [x,y,z(eventually)]
+    startingLocation: list[int] = field(default_factory=list)
+    destroyedLocation: list[int] = field(default_factory=list)
+    currentLocation: list[int]  = field(default_factory=list)
+    
     
     
     
