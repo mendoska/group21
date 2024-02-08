eb674f00ee5e658fe448b62416f3f5406dd14409
sdegefa <sdegefa2020@fau.edu>
2024-02-08 18:47:30 -0500

    starting testing with new swarm package

--- simulation/swarm_ws/src/pythonScripts/Tests/testSetDroneVelocity.py
+++
@@ -1,7 +0,0 @@
-from CLIFunctions.cliFunctions import setDroneVelocity, getDroneLocation
-
-
-speedMultiplier=.01
-location=getDroneLocation(droneID=1)
-setDroneVelocity(droneID=1, xVelocity=-location["x"]*speedMultiplier,yVelocity=-location["y"]*speedMultiplier,zVelocity=location["z"])
-# setDroneVelocity(droneID=0, xVelocity=40,yVelocity=-10)
