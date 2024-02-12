d15ab939a94beabef827e5bdf9a7b3d3dd96d4db
haoranxiao2020 <hxiao2020@fau.edu>
2024-02-12 16:18:19 -0500

    .

--- python/GUI.py
+++ python/GUI.py
@@ -4,10 +4,6 @@ from algorithms.munkres_algorithm import runMunkres
 from algorithms.simulated_annealing import runSimulatedAnnealing
 from tkinter import *
 import customtkinter as ctk
-<<<<<<< Updated upstream
-from PIL import Image
-=======
->>>>>>> Stashed changes
 
 ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
 ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
@@ -16,59 +12,13 @@ root = ctk.CTk()
 root.title('B.O.W.S.E.R.')
 root.geometry('1000x600')
 
-<<<<<<< Updated upstream
-def gaWin():
-   ga = ctk.CTkToplevel(root)
-   ga.title("Genetic Algorithm")
-   ga.geometry("1000x600")
 
-def saWin():
-   sa = ctk.CTkToplevel(root)
-   sa.title("Simulated Annealing")
-   sa.geometry("1000x600")
-
-def maWin():
-   ma = ctk.CTkToplevel(root)
-   ma.title("Munkres Algorithm")
-   ma.geometry("1000x600")
-
-def dtrainWin():
-   dtrain = ctk.CTkToplevel(root)
-   dtrain.title("Deep Q-Learning (Train)")
-   dtrain.geometry("1000x600")
-
-def dtestWin():
-   dtest = ctk.CTkToplevel(root)
-   dtest.title("Deep Q-Learning (Test)")
-   dtest.geometry("1000x600")
-
-
-logo = ctk.CTkImage(light_image=Image.open('BOWSER LOGO FINAL.png'),size=(150,150))
-logoLabel = ctk.CTkLabel(root, text="", image = logo).place(x=10,y=10)
-
-title = ctk.CTkLabel(root,text="B.O.W.S.E.R.",font=("Helvetica",50))
-title.pack(pady=40)
-
-gaBut = ctk.CTkButton(root,text="Genetic Algorithm",command=gaWin)
-gaBut.pack(pady=20)
-
-saBut = ctk.CTkButton(root,text="Simulated Annealing",command=saWin)
-saBut.pack(pady=20)
-
-maBut = ctk.CTkButton(root,text="Munkres Algorithm",command=maWin)
-maBut.pack(pady=20)
-
-dtrainBut = ctk.CTkButton(root,text="Deep Q-Learning (Train)",command=dtrainWin)
-dtrainBut.pack(pady=20)
-
-dtestBut = ctk.CTkButton(root,text="Deep Q-Learning (Test)",command=dtestWin)
-dtestBut.pack(pady=20)
-=======
 def submit():
     rangemin = rminEntry.get()
     print(rangemin)
     rangemax = rmaxEntry.get()
     print(rangemax)
+
 
 rLabel = ctk.CTkLabel(root, text="Range")
 rLabel.pack(pady=5)
@@ -79,7 +29,5 @@ rmaxEntry.pack(pady=10)
 rButton = ctk.CTkButton(root,text="Submit",command=submit)
 rButton.pack(pady=5)
 
-
->>>>>>> Stashed changes
 
 root.mainloop()
