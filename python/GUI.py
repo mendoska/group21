8215d62c1d6fd73c998a1a64f233fd5e2668e5b1
haoranxiao2020 <hxiao2020@fau.edu>
2024-02-12 10:01:42 -0500

    Title and logo added

--- python/GUI.py
+++ python/GUI.py
@@ -4,7 +4,7 @@ from algorithms.munkres_algorithm import runMunkres
 from algorithms.simulated_annealing import runSimulatedAnnealing
 from tkinter import *
 import customtkinter as ctk
-
+from PIL import Image
 
 ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
 ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
@@ -38,19 +38,26 @@ def dtestWin():
    dtest.title("Deep Q-Learning (Test)")
    dtest.geometry("1000x600")
 
+
+logo = ctk.CTkImage(light_image=Image.open('BOWSER LOGO FINAL.png'),size=(150,150))
+logoLabel = ctk.CTkLabel(root, text="", image = logo).place(x=10,y=10)
+
+title = ctk.CTkLabel(root,text="B.O.W.S.E.R.",font=("Helvetica",50))
+title.pack(pady=40)
+
 gaBut = ctk.CTkButton(root,text="Genetic Algorithm",command=gaWin)
-gaBut.pack(pady=40)
+gaBut.pack(pady=20)
 
 saBut = ctk.CTkButton(root,text="Simulated Annealing",command=saWin)
-saBut.pack(pady=40)
+saBut.pack(pady=20)
 
 maBut = ctk.CTkButton(root,text="Munkres Algorithm",command=maWin)
-maBut.pack(pady=40)
+maBut.pack(pady=20)
 
 dtrainBut = ctk.CTkButton(root,text="Deep Q-Learning (Train)",command=dtrainWin)
-dtrainBut.pack(pady=40)
+dtrainBut.pack(pady=20)
 
 dtestBut = ctk.CTkButton(root,text="Deep Q-Learning (Test)",command=dtestWin)
-dtestBut.pack(pady=40)
+dtestBut.pack(pady=20)
 
 root.mainloop()
