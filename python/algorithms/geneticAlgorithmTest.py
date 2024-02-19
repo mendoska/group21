5ae21f8a92f008dce963568ba7c4b6efd5726441
Liliana Ramos <124188462+lylyramos@users.noreply.github.com>
2024-02-18 20:57:23 -0500

    Added cooldown to genetic algorithm

--- python/algorithms/geneticAlgorithmTest.py
+++ python/algorithms/geneticAlgorithmTest.py
@@ -3,6 +3,7 @@ import numpy as np
 from random import choices
 import math
 import pygad
+import time 
 
 
 def runGA(threatFileLocation):
@@ -39,6 +40,10 @@ def runGA(threatFileLocation):
     def fitness_func(ga_instance, solution, solution_idx):
         success_count = 0
         weapons_quantity = [8, 20, 25, 10]
+        #Initiate last fire times for each weapon
+        last_fire_times = [0] * len(weapons_quantity)
+        #Cooldown times in seconds
+        cooldown_time = 5
         for threat in range(len(function_inputs)):
             if solution[threat] == 0:
                 solution[threat] = 1
@@ -50,6 +55,14 @@ def runGA(threatFileLocation):
                 check = float(inputs_position[threat][3])
                 if curr_distance < float(inputs_position[threat][3]):
                     continue
+    #Cooldown system implementation
+    #Get current time
+                current_time = time.time()
+    #Check if the cooldown time has passed
+                if current_time < last_fire_times[solution[threat] - 1] < cooldown_time:
+                    continue
+                else:
+                    last_fire_times[solution[threat] - 1] = current_time
     # Weapon quantity constraint implementation
                 if weapons_quantity[solution[threat]-1] <= 0:
                     continue
