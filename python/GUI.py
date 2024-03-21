7e7c0b9dd14cb06a55ecffa6f98b90b06b42f7c5
haoranxiao2020 <hxiao2020@fau.edu>
2024-03-21 08:29:19 -0400

    range scale

--- python/GUI.py
+++ python/GUI.py
@@ -98,7 +98,7 @@ def submit():
         time.sleep(3)
         response, leaker_percentage = runDQN(loadPath=dqnModelPath, train=False, threatFilePath=threatFileLocation)
         outputLabel.configure(text=f"Leaker Percentage: {leaker_percentage:.2f}%")
-        dleaker.configure(text=f"Deep Q-Learning: \n{leaker_percentage:.2f}%")
+        #dleaker.configure(text=f"Deep Q-Learning: \n{leaker_percentage:.2f}%")
         print(response)
         
 
@@ -106,19 +106,19 @@ def submit():
         response, leaker_percentage = runGA(threatFileLocation=threatFileLocation)
         leaker_percentage = (1.00 - leaker_percentage) * 100
         outputLabel.configure(text=f"Leaker Percentage: {leaker_percentage}%")
-        gleaker.configure(text=f"Genetic Algorithm: \n{leaker_percentage:.2f}%")
+        #gleaker.configure(text=f"Genetic Algorithm: \n{leaker_percentage:.2f}%")
 
     elif algorithm == "Munkres":
         response,leaker_percentage = runMunkres(threatFileLocation=threatFileLocation, weaponFileLocation=weaponFileLocation)
         outputLabel.configure(text=f"Leaker Percentage: {leaker_percentage}%")
-        mleaker.configure(text=f"Munkres Algorithm: \n{leaker_percentage:.2f}%")
+        #mleaker.configure(text=f"Munkres Algorithm: \n{leaker_percentage:.2f}%")
         print(response)
 
     elif algorithm == "Simulated Annealing":
         response,leaker_percentage = runSimulatedAnnealing()
         leaker_percentage = leaker_percentage * 100
         outputLabel.configure(text=f"Leaker Percentage: {leaker_percentage}%")
-        sleaker.configure(text=f"Simulated Annealing: \n{leaker_percentage:.2f}%")
+        #sleaker.configure(text=f"Simulated Annealing: \n{leaker_percentage:.2f}%")
         print(response)
     
     save_list = [algorithm,leaker_percentage]
@@ -189,11 +189,22 @@ logoLabel = ctk.CTkLabel(root, text="", image = logo).place(x=10,y=10)
 title = ctk.CTkLabel(root,text="B.O.W.S.E.R.",font=("Rockwell Extra Bold",50))
 title.pack(pady=40)
 
+canvas = tk.Canvas(root, width=500, height=500, borderwidth=0, highlightthickness=0,bg="black")
+canvas.pack(pady=20)
+canvas.place(x=720,y=200)
+
+def _create_circle(self, x, y, r, **kwargs):
+    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+tk.Canvas.create_circle = _create_circle
+
+canvas.create_circle(250, 250, 10, fill="#BBB", outline="")
+
+
 def showRange():
-    canvas = tk.Canvas(root, width=300, height=300, borderwidth=0, highlightthickness=0,
+    canvas = tk.Canvas(root, width=500, height=500, borderwidth=0, highlightthickness=0,
                     bg="black")
     canvas.pack(pady=20)
-    canvas.place(x=900,y=400)
+    canvas.place(x=720,y=200)
 
     rangemin = int(rminEntry.get())
     rangemax = int(rmaxEntry.get())
@@ -202,6 +213,20 @@ def showRange():
 
     startAngle = 90 - (angle/2)
     endAngle = 90 + (angle/2)
+    rSize = 10
+
+    if rangemax > 900:
+        rangemax /= 9
+        rangemin /= 9
+        rSize /= 9
+        unitLabel.configure(text=f"|______| 450 {unit}")
+    elif rangemax > 300:
+        rangemax /= 3
+        rangemin /= 3
+        rSize /= 3
+        unitLabel.configure(text=f"|______| 150 {unit}")
+    else:
+        unitLabel.configure(text=f"|______| 50 {unit}")
 
     def _create_circle(self, x, y, r, **kwargs):
         return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
@@ -213,30 +238,43 @@ def showRange():
         return self.create_arc(x-r, y-r, x+r, y+r, **kwargs)
     tk.Canvas.create_circle_arc = _create_circle_arc
 
-    canvas.create_circle_arc(150, 150, rangemax, fill="green", outline="", start=startAngle, end=endAngle)
-    canvas.create_circle_arc(150, 150, rangemin, fill="red", outline="", start=startAngle, end=endAngle)
-    canvas.create_circle(150, 150, 10, fill="#BBB", outline="")
+    canvas.create_circle_arc(250, 250, rangemax, fill="green", outline="", start=startAngle, end=endAngle)
+    canvas.create_circle_arc(250, 250, rangemin, fill="red", outline="", start=startAngle, end=endAngle)
+    canvas.create_circle(250, 250, rSize, fill="#BBB", outline="")
 
-    unitLabel.configure(text=f"|______| 50 {unit}")
-    #unitLabel.pack(pady=2)
-
-rLabel = ctk.CTkLabel(root, text="Range")
+rLabel = ctk.CTkLabel(root, text="Spawn Range")
 rLabel.pack(pady=5)
+rLabel.place(x=350,y=150)
 rminEntry = ctk.CTkEntry(root, placeholder_text="Min")
 rminEntry.pack(pady=10)
+rminEntry.place(x=320,y=190)
 rmaxEntry = ctk.CTkEntry(root, placeholder_text="Max")
 rmaxEntry.pack(pady=10)
+rmaxEntry.place(x=320,y=230)
 
 angleEntry = ctk.CTkSlider(root, from_=1, to=359, command=angleSlider)
 angleEntry.pack(pady=2)
 angleEntry.set(1)
+angleEntry.place(x=290,y=270)
 angleLabel = ctk.CTkLabel(root, text=f"Angle: {angleEntry.get()} degree")
 angleLabel.pack(pady=2)
+angleLabel.place(x=345,y=290)
+
 rangeButton = ctk.CTkButton(root, text="View Range", command=showRange)
 rangeButton.pack(pady=5)
+rangeButton.place(x=320,y=325)
+
+radarLabel = ctk.CTkLabel(root, text="Visualize Spawn Range")
+radarLabel.pack(pady=5)
+radarLabel.place(x=710,y=130)
+
 unitLabel = ctk.CTkLabel(root, text="")
 unitLabel.pack(pady=5)
-unitLabel.place(x=720, y=560)
+unitLabel.place(x=900, y=560)
+
+scaleLabel = ctk.CTkLabel(root, text="|______|_______|_______|_______|______")
+scaleLabel.pack(pady=5)
+scaleLabel.place(x=577, y=560)
 
 #minVar = tk.IntVar(value=1)
 #rminEntry = ctk.CTkSlider(root, from_=10, to=50, variable=minVar,command=minSlider,progress_color="red")
@@ -252,41 +290,46 @@ unitLabel.place(x=720, y=560)
 unitOptions = ["m", "km", "ft", "mi"]
 unitDropdown = ctk.CTkOptionMenu(root, values=unitOptions,width=60)
 unitDropdown.pack(pady=10)
-unitDropdown.place(x=600,y=210)
+unitDropdown.place(x=480,y=210)
 
 algoLabel = ctk.CTkLabel(root, text="Select Algorithm")
 algoLabel.pack(pady=5)
+algoLabel.place(x=345,y=370)
 algoOptions = ["DQN", "Genetic Algorithm", "Munkres", "Simulated Annealing"]
 algoDropdown = ctk.CTkOptionMenu(root, values=algoOptions)
 algoDropdown.pack(pady=10)
+algoDropdown.place(x=320,y=410)
 
 threatVar = tk.IntVar(value=1)
 threatScale = ctk.CTkSlider(root, from_=1, to=10, variable=threatVar, command=updateThreatLabel)
 threatScale.pack(pady=10)
+threatScale.place(x=290,y=450)
 currentThreatLabel = ctk.CTkLabel(root, text=f"Current Number of Threats: {threatVar.get()}")
 currentThreatLabel.pack(pady=5)
+currentThreatLabel.place(x=310,y=470)
 
 submitButton = ctk.CTkButton(root, text="Start", command=submit)
 submitButton.pack(pady=5)
+submitButton.place(x=320,y=510)
 
 outputLabel = ctk.CTkLabel(root, text="", wraplength=800)
 outputLabel.pack(side="bottom", pady=10)
 
-dleaker = ctk.CTkLabel(root, text="Deep Q-Learning: No Tracked Runs in Current Session", wraplength=200)
-dleaker.pack(side="right")
-dleaker.place(x=800,y=150)
+#dleaker = ctk.CTkLabel(root, text="Deep Q-Learning: No Tracked Runs in Current Session", wraplength=200)
+#dleaker.pack(side="right")
+#dleaker.place(x=800,y=150)
 
-gleaker = ctk.CTkLabel(root, text="Genetic Algorithm: No Tracked Runs in Current Session", wraplength=200)
-gleaker.pack(side="right")
-gleaker.place(x=800,y=190)
+#gleaker = ctk.CTkLabel(root, text="Genetic Algorithm: No Tracked Runs in Current Session", wraplength=200)
+#gleaker.pack(side="right")
+#gleaker.place(x=800,y=190)
 
-mleaker = ctk.CTkLabel(root, text="Munkres Algorithm: No Tracked Runs in Current Session", wraplength=200)
-mleaker.pack(side="right")
-mleaker.place(x=800,y=230)
+#mleaker = ctk.CTkLabel(root, text="Munkres Algorithm: No Tracked Runs in Current Session", wraplength=200)
+#mleaker.pack(side="right")
+#mleaker.place(x=800,y=230)
 
-sleaker = ctk.CTkLabel(root, text="Simulated Annealing: No Tracked Runs in Current Session", wraplength=200)
-sleaker.pack(side="right")
-sleaker.place(x=800,y=270)
+#sleaker = ctk.CTkLabel(root, text="Simulated Annealing: No Tracked Runs in Current Session", wraplength=200)
+#sleaker.pack(side="right")
+#sleaker.place(x=800,y=270)
 
 leaderBoard = ctk.CTkLabel(root, text="\n".join(savedList.split('\n')[:10]), anchor="e", justify=RIGHT, wraplength=400)
 leaderBoard.pack(side="left")
